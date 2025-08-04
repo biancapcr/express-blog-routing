fdb927ce08e6343213431036471fee70e895b1bd
biancapcr <biancacristina.pocora@gmail.com>
2025-08-04 16:01:46 +0200

    [upd] enable static assets

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 //console.log("Hello World");
 // importo express
 const express = require('express');
+
 // creo l’applicazione
 const app = express();
+
 // imposto la porta
 const port = 3000;
+
+// abilito asset statici dalla cartella “public”
+app.use(express.static('public'));
+
 // definizione rotta base
 app.get('/', (req, res) => {
   res.send('Benvenuto nel mio blog');
 });
+
 // avvio server
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);

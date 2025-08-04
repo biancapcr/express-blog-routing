8e2eb22302ab54e01b271ce08cbba2bf44459b29
biancapcr <biancacristina.pocora@gmail.com>
2025-08-04 15:57:48 +0200

    [upd] import express, [add] app [add] port and main endpoint

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//console.log("Hello World");
+// importo express
+const express = require('express');
+// creo l’applicazione
+const app = express();
+// imposto la porta
+const port = 3000;
+// definizione rotta base
+app.get('/', (req, res) => {
+  res.send('Benvenuto nel mio blog');
+});
